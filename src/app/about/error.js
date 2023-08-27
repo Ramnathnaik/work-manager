998f73712e218696697bf64096693e0c17fb5a0c
Ramanath K P <ramnathnaik447@gmail.com>
2023-08-27 12:09:04 +0530

    Deleted About page

--- src/app/about/error.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ErrorPage({error, reset}) {
-    return (
-        <div>
-            <h1>There is an error in about page</h1>
-        </div>
-    );
-}
--- src/app/about/loading.js
+++
@@ -1,7 +0,0 @@
-export default function LoadingLayout() {
-    return (
-        <div>
-            <h1>Currently the content is being loaded</h1>
-        </div>
-    );
-}
