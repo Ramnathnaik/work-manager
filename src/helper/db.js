1ed3d2e048dc72d94780d87f8715e12b053739ef
Ramanath K P <ramnathnaik447@gmail.com>
2023-08-27 11:44:08 +0530

    Bug fix in db connection

--- src/helper/db.js
+++ src/helper/db.js
@@ -2,16 +2,17 @@ import { User } from "@/models/user";
 import mongoose from "mongoose";
 
 export async function connectDB() {
-    try 
-    {
-        const {connection} = await mongoose.connect(process.env.MONGO_DB_URL, {
-            dbName: 'work_manager'
-        });
-
-        console.log('db connected..');
+  try {
+    if (mongoose.connection.readyState !== 1) {
+      console.log("inside");
+      const { connection } = await mongoose.connect(process.env.MONGO_DB_URL, {
+        dbName: "work_manager",
+      });
+      config.isConnectionReady = connection.readyState;
+      console.log("db connected..");
     }
-    catch (error) {
-        console.log('connection failed');
-        // console.log(error);
-    }
+  } catch (error) {
+    console.log("connection failed");
+    // console.log(error);
+  }
 }
